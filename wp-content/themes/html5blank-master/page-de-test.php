5ff0898a001f9ef69f9aedec73f323eddaf74655
kayzor <m67.widemann@gmail.com>
2014-04-17 14:30:16 +0200

    Bases

---
+++ wp-content/themes/html5blank-master/page-de-test.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="fr-FR" class="no-js">
+	<head>
+		<meta charset="UTF-8">
+		<title> Pimp my lens</title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="http://localhost/pimpmylens/wp-content/themes/html5blank-master/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="http://localhost/pimpmylens/wp-content/themes/html5blank-master/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Un site utilisant WordPress">
+
+		<meta name='robots' content='noindex,follow' />
+<link rel='stylesheet' id='normalize-css'  href='http://localhost/pimpmylens/wp-content/themes/html5blank-master/normalize.css?ver=1.0' media='all' />
+<link rel='stylesheet' id='html5blank-css'  href='http://localhost/pimpmylens/wp-content/themes/html5blank-master/style.css?ver=1.0' media='all' />
+<script type='text/javascript' src='http://localhost/pimpmylens/wp-content/themes/html5blank-master/js/lib/conditionizr-4.3.0.min.js?ver=4.3.0'></script>
+<script type='text/javascript' src='http://localhost/pimpmylens/wp-content/themes/html5blank-master/js/lib/modernizr-2.7.1.min.js?ver=2.7.1'></script>
+<script type='text/javascript' src='http://localhost/pimpmylens/wp-includes/js/jquery/jquery.js?ver=1.10.2'></script>
+
+<script type='text/javascript' src='http://localhost/pimpmylens/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='http://localhost/pimpmylens/wp-content/themes/html5blank-master/js/scripts.js?ver=1.0.0'></script>
+		<script>
+        // conditionizr.com
+        // configure environment tests
+        conditionizr.config({
+            assets: 'http://localhost/pimpmylens/wp-content/themes/html5blank-master',
+            tests: {}
+        });
+        </script>
+
+	</head>
+	<body class="home logged-in">
+
+
+		<header>
+			<div class="banniere">
+				Ici la banniere
+
+			</div>
+			<nav>
+				<ul>
+					<li>Item 1...</li>
+				</ul>
+			</nav>
+		</header>
+
+		<aside>
+			Liste des catégories
+		</aside>
+
+		<section class="contenu">
+			<article>
+				Ici un article...
+			</article>
+
+		</section>
+
+		<footer>
+
+			Ici le footer
+		</footer>
+
+
+		ICI mon site
+
+	</body>
+
+</html>
